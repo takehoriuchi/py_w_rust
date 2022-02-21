5cd9ffcfe996f4c7d2c7b20d3cef9042308dabe2
Takehito Horiuchi <horiuchi@brains.co.jp>
2022-02-21 15:36:27 +0900

    modify __init__.py

--- flitton_fib_py/__init__.py
+++ flitton_fib_py/__init__.py
@@ -3,3 +3,6 @@ from fib_numbers import *
 
 def say_hello() -> None:
     print("the Flitton Fibonacci module is saying hello")
+
+def say_goodbye() -> None:
+    print("the Flitton Fibonacci module is saying hello")
